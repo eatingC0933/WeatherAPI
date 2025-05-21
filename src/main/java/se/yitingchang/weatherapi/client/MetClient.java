3379b95311917153887a49100e70e743ae34a868
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:34:10 +0200

    Update MetClient.java

--- src/main/java/se/yitingchang/weatherapi/client/MetClient.java
+++ src/main/java/se/yitingchang/weatherapi/client/MetClient.java
@@ -1,14 +1,19 @@
-package se.yitingchang.weatherapi.service;
+package se.yitingchang.weatherapi.client;
 
-import org.springframework.stereotype.Service;
+
+import org.springframework.stereotype.Component;
+
 import org.springframework.web.reactive.function.client.WebClient;
+import se.yitingchang.weatherapi.model.Metpojos.MetResponse;
+import se.yitingchang.weatherapi.model.Metpojos.Timeseries;
 import se.yitingchang.weatherapi.model.WeatherData;
-import se.yitingchang.weatherapi.model.pojo.MetResponse;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 
-@Service
+@Component
 public class MetClient {
     private final WebClient webClient=WebClient.create();
 
@@ -19,13 +24,32 @@ public class MetClient {
                 .bodyToMono(MetResponse.class)
                 .block();
 
-        return extract24hForecast(response);
-    }
 
-    private WeatherData extract24hForecast(MetResponse response) {
-        String timestampStr = response.getTime();
-        LocalDateTime timestamp = LocalDateTime.parse(timestampStr, DateTimeFormatter.ISO_DATE_TIME);
+        if (response == null || response.getProperties() == null || response.getProperties().getTimeseries() == null) {
+            throw new RuntimeException("No forecast data received from MET Norway");
+        }
 
-        return new WeatherData("MET",response.getAirTemperature(), response.getRelativeHumidity(),timestamp);
+        LocalDateTime targetTime = LocalDateTime.now().plusHours(24);
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+        Timeseries closest = response.getProperties().getTimeseries().stream()
+                .min(Comparator.comparing(ts -> Duration.between(
+                        LocalDateTime.parse(ts.getTime(), formatter),
+                        targetTime).abs())).orElseThrow();
+
+        double temperature = closest.getData().getInstant().getDetails().getAirTemperature();
+        double humidity=closest.getData().getInstant().getDetails().getRelativeHumidity();
+        LocalDateTime timestamp = LocalDateTime.parse(closest.getTime(), formatter);
+
+        WeatherData weatherData = new WeatherData();
+        weatherData.setSource("MET");
+        weatherData.setTemperature(temperature);
+        weatherData.setHumidity(humidity);
+        weatherData.setTimestamp(timestamp);
+
+        return weatherData;
+
+
     }
 }
+
