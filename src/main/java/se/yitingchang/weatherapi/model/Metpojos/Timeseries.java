0c9e7fc99e8e4807113ee19fd33b3986258910f3
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:25:09 +0200

    Update Timeseries.java

--- src/main/java/se/yitingchang/weatherapi/model/Metpojos/Timeseries.java
+++ src/main/java/se/yitingchang/weatherapi/model/Metpojos/Timeseries.java
@@ -1,4 +1,54 @@
 package se.yitingchang.weatherapi.model.Metpojos;
 
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "time",
+        "data"
+})
+@Generated("jsonschema2pojo")
 public class Timeseries {
+
+    @JsonProperty("time")
+    private String time;
+    @JsonProperty("data")
+    private Data data;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("time")
+    public String getTime() {
+        return time;
+    }
+
+    @JsonProperty("time")
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @JsonProperty("data")
+    public Data getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
