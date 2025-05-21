d9c44d606470fac670891b074f1bd6246f86a5ef
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:26:24 +0200

    Update LevelSeries.java

--- src/main/java/se/yitingchang/weatherapi/model/Pollenpojos/LevelSeries.java
+++ src/main/java/se/yitingchang/weatherapi/model/Pollenpojos/LevelSeries.java
@@ -1,4 +1,71 @@
 package se.yitingchang.weatherapi.model.Pollenpojos;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "pollenId",
+        "level",
+        "time"
+})
+@Generated("jsonschema2pojo")
 public class LevelSeries {
+
+    @JsonProperty("pollenId")
+    private String pollenId;
+    @JsonProperty("level")
+    private Integer level;
+    @JsonProperty("time")
+    private String time;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("pollenId")
+    public String getPollenId() {
+        return pollenId;
+    }
+
+    @JsonProperty("pollenId")
+    public void setPollenId(String pollenId) {
+        this.pollenId = pollenId;
+    }
+
+    @JsonProperty("level")
+    public Integer getLevel() {
+        return level;
+    }
+
+    @JsonProperty("level")
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    @JsonProperty("time")
+    public String getTime() {
+        return time;
+    }
+
+    @JsonProperty("time")
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
