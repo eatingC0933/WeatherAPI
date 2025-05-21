ee751de81ceea09ac67339647dc55bfbf332df7c
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:30:00 +0200

    Update WeatherData.java

--- src/main/java/se/yitingchang/weatherapi/model/WeatherData.java
+++ src/main/java/se/yitingchang/weatherapi/model/WeatherData.java
@@ -1,12 +1,33 @@
-package se.yitingchang.weatherapi;
+package se.yitingchang.weatherapi.model;
+
 
 import java.time.LocalDateTime;
 
 public class WeatherData {
     private String source;
     private double temperature;
-    private int humidity;
+    private double humidity;
     private LocalDateTime timestamp;
+
+    public WeatherData(String source, double temperature, double humidity, LocalDateTime timestamp) {
+        this.source = source;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.timestamp = timestamp;
+    }
+
+    public WeatherData() {
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "source='" + source + '\'' +
+                ", temperature=" + temperature +
+                ", humidity=" + humidity +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
 
     public String getSource() {
         return source;
@@ -24,9 +45,22 @@ public class WeatherData {
         this.temperature = temperature;
     }
 
-    public int getHumidity() {
+    public double getHumidity() {
         return humidity;
     }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
 
     public void setHumidity(int humidity) {
         this.humidity = humidity;
