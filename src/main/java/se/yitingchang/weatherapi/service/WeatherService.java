31dc18432b85f0b178ed732b8e683fbb8dc55216
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:31:27 +0200

    Update WeatherService.java

--- src/main/java/se/yitingchang/weatherapi/service/WeatherService.java
+++ src/main/java/se/yitingchang/weatherapi/service/WeatherService.java
@@ -1,33 +1,49 @@
-package se.yitingchang.weatherapi.client;
+package se.yitingchang.weatherapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.yitingchang.weatherapi.client.MetClient;
+import se.yitingchang.weatherapi.client.SmhiClient;
 import se.yitingchang.weatherapi.model.WeatherData;
+
 
 @Service
 public class WeatherService {
-    @Autowired
     private final SmhiClient smhiClient;
-    @Autowired
     private final MetClient metClient;
 
+    @Autowired
     public WeatherService(SmhiClient smhiClient, MetClient metClient) {
         this.smhiClient = smhiClient;
         this.metClient = metClient;
     }
 
+    public WeatherData getSmhiForecast() {
+        return smhiClient.fetchForecast();
+    }
+
+    public WeatherData getMetForecast() {
+        return metClient.fetchForecast();
+    }
 
     public WeatherData getOptimalForecast() {
-        WeatherData data1 = smhiClient.fetchForecast();
-        WeatherData data2 = metClient.fetchForecast();
-
-        return chooseBestWeather(data1, data2);
+        WeatherData smhi = getSmhiForecast();
+        WeatherData met = getMetForecast();
+        return chooseBestWeather(smhi, met);
     }
 
-    private WeatherData chooseBestWeather(WeatherData w1, WeatherData w2) {
+    //Choose the best weather by "Score = temperature - (humidity / 2)"
+    public WeatherData chooseBestWeather(WeatherData w1, WeatherData w2) {
         if (w1 == null) return w2;
         if (w2 == null) return w1;
-        return (w1.getTemperature() > w2.getTemperature() && w1.getHumidity() < w2.getHumidity()) ? w1 : w2;
+
+        double score1 = w1.getTemperature() - (w1.getHumidity() / 2.0);
+        double score2 = w2.getTemperature() - (w2.getHumidity() / 2.0);
+
+        return score1 >= score2 ? w1 : w2;
     }
+
 }
+
+
 
