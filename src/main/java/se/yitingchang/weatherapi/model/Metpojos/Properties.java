3a04900e410c6d5756ba2c0282248a71db17a6d7
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:24:43 +0200

    Update Properties.java

--- src/main/java/se/yitingchang/weatherapi/model/Metpojos/Properties.java
+++ src/main/java/se/yitingchang/weatherapi/model/Metpojos/Properties.java
@@ -1,4 +1,42 @@
 package se.yitingchang.weatherapi.model.Metpojos;
 
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "timeseries"
+})
+@Generated("jsonschema2pojo")
 public class Properties {
+
+    @JsonProperty("timeseries")
+    private List<Timeseries> timeseries;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("timeseries")
+    public List<Timeseries> getTimeseries() {
+        return timeseries;
+    }
+
+    @JsonProperty("timeseries")
+    public void setTimeseries(List<Timeseries> timeseries) {
+        this.timeseries = timeseries;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
