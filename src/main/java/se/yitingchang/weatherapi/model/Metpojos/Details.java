b6b7fced15315f389eca554a7ed3fb3921e60774
Yiting Chang <yiting.chang@iths.se>
2025-05-21 13:23:14 +0200

    Update Details.java

--- src/main/java/se/yitingchang/weatherapi/model/Metpojos/Details.java
+++ src/main/java/se/yitingchang/weatherapi/model/Metpojos/Details.java
@@ -1,4 +1,54 @@
 package se.yitingchang.weatherapi.model.Metpojos;
 
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "air_temperature",
+        "relative_humidity"
+})
+@Generated("jsonschema2pojo")
 public class Details {
+
+    @JsonProperty("air_temperature")
+    private Double airTemperature;
+    @JsonProperty("relative_humidity")
+    private Double relativeHumidity;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("air_temperature")
+    public Double getAirTemperature() {
+        return airTemperature;
+    }
+
+    @JsonProperty("air_temperature")
+    public void setAirTemperature(Double airTemperature) {
+        this.airTemperature = airTemperature;
+    }
+
+    @JsonProperty("relative_humidity")
+    public Double getRelativeHumidity() {
+        return relativeHumidity;
+    }
+
+    @JsonProperty("relative_humidity")
+    public void setRelativeHumidity(Double relativeHumidity) {
+        this.relativeHumidity = relativeHumidity;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
